b4460735ddd79b7da7221d97fd71c61e6cabbb68
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-07-17 11:31:18 -0400

    update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,20 @@
 let url = "travel_recommendation_api.json";
-fetch(url)
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
+let sbar= document.getElementById("search");
+let sbtn= document.getElementById("searchBtn1");
+function search1(){
+    if (!sbar.value) return;
+    let val = sbar.value.toLowerCase();
+    console.log(val);
+    if (val ==='beach' || val === 'beaches') val ="beach";
+    else if (val === 'country' || val === 'countries') val ="country";
+    else if (val === 'temple' || val === 'temples') val ="temple";
+    else val = ''
+    console.log(val);
+    if (!val) return;
+    // fetch(url)
+    // .then(response=>response.json())
+    // .then(data=>{
 
-})
+    // })
+}
+sbtn.addEventListener('click', search1)
