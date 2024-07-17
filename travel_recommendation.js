f8c4953707c914bf28e99a49db9615120e97e938
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-07-17 14:43:33 -0400

    update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,13 +2,19 @@ let url = "travel_recommendation_api.json";
 let sbar= document.getElementById("search");
 let sbtn= document.getElementById("searchBtn");
 function search(){
-    if (!sbar.value) return;
+    if (!sbar.value){
+        clear();
+        return;
+    }
     let val = sbar.value.toLowerCase();
     if (val ==='beach' || val === 'beaches') val ="beaches";
     else if (val === 'country' || val === 'countries') val ="countries";
     else if (val === 'temple' || val === 'temples') val ="temples";
     else val = ''
-    if (!val) return;
+    if (!val){
+        clear();
+        return;
+    }
     fetch(url)
     .then(response=>response.json())
     .then(data=>{
@@ -34,6 +40,18 @@ function search(){
                 })
                 inh+=`<br>`
             })
+        }
+        else{
+            result.forEach(i=>{
+                inh+=`<br>`
+                // inh+=`<img src=${i["imageUrl"]} style="width: 400px; height:auto" >`;
+                inh+=`<img src=${i["imageUrl"]} style="max-width: 800px; height:auto">`;
+                inh+=`<h3>City:${i["name"]}<h3>`;
+                // inh+=`<p style="width: 400px;font-weight:100 ;font-size: medium;">Description:<span>${i["description"]}</span><p>`
+                inh+=`<h5>${i["description"]}</h5>`
+                inh+=`<button style="height:50px;width:150px; color: white; background-color: cadetblue; border-radius: 5px; border: none;">VISIT</button><br><br><br><br>`
+
+                })
         }
         // elem.setAttribute("style", style);
         // elem.setAttribute("id", id);
